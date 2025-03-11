91acb7a22068614f42c978a0c0748680bd43951f
AnkitBareth <ankitbareth7877@gmail.com>
2025-03-11 12:32:10 +0530

    add user register validation

--- src/user/userControllers.ts
+++ src/user/userControllers.ts
@@ -1,6 +1,20 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  //Get the data from request
+
+  const { name, email, password } = req.body;
+
+  //Validation
+
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "Please enter all fields");
+    return next(error);
+  }
+  //Process the request
+  //Send the response
+
   res.json({ message: "User registered successfully" });
 };
 
